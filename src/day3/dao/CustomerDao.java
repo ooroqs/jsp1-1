2e6134edd96f0a20b28ccc3ee09a847dd14564d3
DESKTOP-R2NKGM0\admin <koreait.2021@gmail.com>
2021-09-23 09:52:27 +0900

    day4 update dao

--- src/day3/dao/CustomerDao.java
+++ src/day3/dao/CustomerDao.java
@@ -17,6 +17,34 @@ public class CustomerDao {
 		return dao;
 	}
 	
+	public Customer selectOne(int idx) {
+		Customer cus = null;
+		String sql = "select * from customer where idx=?";   //idx 는 pk 컬럼
+		Connection conn = MySQLConnectionUtil.connect();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, idx);
+			rs=pstmt.executeQuery();
+			if(rs.next()) {
+				cus = new Customer(rs.getInt(1), rs.getString(2), rs.getString(3), 
+						rs.getString(4), rs.getString(5), rs.getString(6), 
+						rs.getInt(7), rs.getString(8));
+			}
+		}catch (SQLException e) {
+			System.out.println("selectOne 오류 : " + e.getMessage());
+		}finally {
+			try {
+				pstmt.close();
+			} catch (SQLException e) {	}
+			MySQLConnectionUtil.close(conn);
+		}
+		
+		return cus;   //idx가 없는 값으로 조회하면 리턴은 null
+	}
+	
+	
 	public List<Customer> selectAll(){
 		List<Customer> list = new ArrayList<Customer>();
 		String sql = "select * from customer";
@@ -46,6 +74,32 @@ public class CustomerDao {
 		}
 		return list;
 	}
+	
+	public void update(Customer cus) {
+		String sql ="update customer set email=?,addr=? where idx=?";
+	
+		Connection conn = MySQLConnectionUtil.connect();
+		PreparedStatement pstmt = null;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, cus.getEmail());
+			pstmt.setString(2, cus.getAddr());
+			pstmt.setInt(3, cus.getIdx());
+			pstmt.execute();
+			conn.commit();
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+			System.out.println("update 오류 : " + e.getMessage());
+		}finally {
+			try {
+				pstmt.close();
+			} catch (SQLException e1) {
+			}
+			MySQLConnectionUtil.close(conn);
+		}
+	}
+	
 	
 	
 	public void insert(Customer cus) {
